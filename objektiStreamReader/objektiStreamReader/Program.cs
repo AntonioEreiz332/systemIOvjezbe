f60906a435530dbf7e05651e33178aa9bb093d2d
AntonioEreiz332 <58472078+AntonioEreiz332@users.noreply.github.com>
2021-04-10 22:19:33 +0200

    Add files via upload

---
+++ objektiStreamReader/objektiStreamReader/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace objektiStreamReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(@"D:\My Documents\NOOP\datoteka.txt");
+            // Čitamo datoteku liniju po liniju
+            while (!sr.EndOfStream)
+            {
+                Console.WriteLine(sr.ReadLine());
+            }
+            // Zatvaramo datoteku
+            sr.Close();
+            Console.ReadKey();
+        }
+    }
+}
