8f203bd936c5d3882937bc1f96475aadea890f7b
AntonioEreiz332 <58472078+AntonioEreiz332@users.noreply.github.com>
2021-04-10 22:29:53 +0200

    Add files via upload

---
+++ naredbaUsing/naredbaUsing/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace naredbaUsing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Unesite ime: ");
+            string ime = Console.ReadLine();
+            Console.Write("Unesite prezime: ");
+            string prezime = Console.ReadLine();
+            // Podatke upisujemo u datoteku
+            Console.WriteLine("\n-- Zapisujemo u datoteku...");
+            using (StreamWriter sw = new StreamWriter(@"D:\My Documents\NOOP\datoteka.txt"))
+            {
+                sw.WriteLine("Ime: { 0}", ime);
+                sw.WriteLine("Prezime: { 0}", prezime);
+            }
+        }
+    }
+}
