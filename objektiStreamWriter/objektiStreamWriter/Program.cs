818cf76c3601cf3c3960181f2718f78eb2de1fdc
AntonioEreiz332 <58472078+AntonioEreiz332@users.noreply.github.com>
2021-04-10 22:17:44 +0200

    Add files via upload

---
+++ objektiStreamWriter/objektiStreamWriter/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace objektiStreamWriter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Unesite ime: ");
+            string ime = Console.ReadLine();
+            Console.Write("Unesite prezime: ");
+            string prezime = Console.ReadLine();
+            StreamWriter sw = new StreamWriter(@"D:\My Documents\NOOP\datoteka.txt");
+            sw.WriteLine("Ime: { 0}", ime);
+            sw.WriteLine("Prezime: { 0}", prezime);
+            sw.Close();
+        }
+    }
+}
